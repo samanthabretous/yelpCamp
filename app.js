0257a2add4bfe80b9b017b3dd55610eacf046010
Samantha Bretous <sam@samanthabretous.com>
2016-07-12 14:00:57 -0400

    moved campground templates to new directory. added comments get and post routes

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ var express     = require("express"),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
     Campground  = require("./models/campground") , 
+    Comment     = require("./models/comment")
     seedDB      = require("./seeds")
 
-seedDB()
+
 mongoose.connect("mongodb://localhost/yelp_camp")
 app.use(bodyParser.urlencoded({extended: true}))
 app.set("view engine", "ejs")
-
+seedDB()
 
 
 app.get("/", function(req, res){
@@ -23,7 +24,7 @@ app.get("/campgrounds", function(req, res){
     if(err){
       console.log("ERROR")
     } else {
-      res.render("index", {campgrounds: allCampgrounds})
+      res.render("campgrounds/index", {campgrounds: allCampgrounds})
     }
   })
 })
@@ -48,7 +49,7 @@ app.post("/campgrounds", function(req, res){
 
 //NEW route - show form to create new campground
 app.get("/campgrounds/new", function(req, res){
-  res.render("new.ejs")
+  res.render("campgrounds/new")
 })
 
 //SHOW route - info about one item
@@ -60,7 +61,45 @@ app.get("/campgrounds/:id", function(req, res){
     } else {
       console.log(foundCampground)
       //render show template with that campground
-      res.render("show", {campground: foundCampground})
+      res.render("campgrounds/show", {campground: foundCampground})
+    }
+  })
+})
+
+
+//==========================
+//COMMENTS ROUTE
+//==========================
+app.get("/campgrounds/:id/comments/new", function(req, res){
+  //find campground by id
+  Campground.findById(req.params.id, function(err, campground){
+    if(err){
+      console.log(err)
+    } else {
+      res.render("comments/new", {campground: campground})
+    }
+  })
+})
+
+app.post("/campgrounds/:id/comments", function(req, res){
+  //lookup campground using ID
+  Campground.findById(req.params.id, function(err, campground){
+    if(err) {
+      console.log(err)
+      res.redirect("/campground")
+    } else {
+      //create new comment
+      Comment.create(req.body.comment, function(err, comment){
+        if(err){
+          console.log(err)
+        } else {
+          //connect new campground
+          campground.comments.push(comment)
+          campground.save()
+          //redirect to campground show page
+          res.redirect("/campgrounds/" +campground._id)
+        }
+      })
     }
   })
 })
